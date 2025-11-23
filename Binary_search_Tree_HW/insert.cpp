d5182980f9450a5d0e53975cf780ac3b1ccf20bb
Svetlana Zakharyan <svetlana.zakharyan@student.rau.am>
2025-11-23 14:27:40 +0400

    Create insert.cpp

---
+++ Binary_search_Tree_HW/insert.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+TreeNode* insert(TreeNode* root, int val) {
+    if (!root) {
+        return new TreeNode(val);
+    }
+
+    if (val < root->val) {
+        root->left = insert(root->left, val);
+    } else if (val > root->val) {
+        root->right = insert(root->right, val);
+    } 
+
+    return root;
+}
+
+void inorder(TreeNode* root) {
+    if (!root) return;
+    inorder(root->left);
+    cout << root->val << " ";
+    inorder(root->right);
+}
+
+int main() {
+    TreeNode* root = nullptr;
+    root = insert(root, 5);
+    root = insert(root, 3);
+    root = insert(root, 7);
+    root = insert(root, 1);
+
+    inorder(root);
+}
